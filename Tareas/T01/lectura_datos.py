40f7cb6102534416e345e913d1c752416f248f5b
DOMINGO EDWARDS <domingo.edwards@uc.cl>
2020-09-07 16:17:01 -0300

    creados metodos de lectura de datos

---
+++ Tareas/T01/lectura_datos.py
@@ -0,0 +1,88 @@
+def leer_datos_delegaciones(archivo):
+    """
+    Esta funcion toma un archivo.csv y retorna una lista con diccionarios
+    que contienen los datos de la delegacion como {nombre_dato : dato}
+    """
+    archivo = open(archivo, "r")
+    lista_delegaciones = []  # esta lista contiene los datos de las delegaciones ordenados
+
+    # diccionario que tiene como llaves los label del archivo y como value el dato
+    datos_delegacion = dict()
+    llaves = archivo.readline()[:-1].split(",")
+
+    for llave in llaves:
+        datos_delegacion[llave] = None
+    for linea in archivo.readlines():
+        datos_linea = linea[:-1].split(",")
+
+        COUNT = 0
+        for dato in datos_linea:
+            datos_delegacion[llaves[COUNT]] = dato
+            COUNT += 1
+        lista_delegaciones.append(datos_delegacion)
+
+    return lista_delegaciones
+
+
+def leer_datos_deportistas(archivo):
+    """
+    Esta funcion toma un archivo.csv y retorna una lista con diccionarios
+    que contienen los datos de los deportistas como {nombre_dato : dato}
+    """
+    archivo = open(archivo, "r")
+    lista_deportistas = []  # esta lista contiene los datos de los deportistas
+
+    # diccionario que tiene como llaves los label del archivo y como value el dato
+    datos_deportista = dict()
+    llaves = archivo.readline()[:-1].split(",")
+
+    # crea el diccionario de los datos de 1 deportista {"label_dato" : dato}
+    COUNT = 0
+    for llave in llaves:
+        llaves[COUNT] = llave.strip()
+        llave = llave.strip()
+        datos_deportista[llave] = None
+        COUNT += 1
+
+    # lee el archivo y llena la lista deportistas con los datos
+    for linea in archivo.readlines():
+        datos_deportista_i = datos_deportista.copy()
+        datos_linea = linea[:-1].split(",")
+        print(datos_linea)
+        COUNT = 0
+        for dato in datos_linea:
+            datos_deportista_i[llaves[COUNT]] = dato.strip()
+            COUNT += 1
+        print(datos_deportista_i)
+        print("-" * 150)
+        lista_deportistas.append(datos_deportista_i)
+
+    return lista_deportistas
+
+
+if __name__ == "__main__":
+    from clases_simulacion import DCCrotona, IEEEsparta, Deportista
+
+    # Testeo delegaciones.csv
+    d1 = Deportista("Alexis", 14, 20, 30, 88, False, 20)
+    d2 = Deportista("Charles", 15, 23, 43, 50, True, 23)
+    d3 = Deportista("Mago Valdivia", 23, 34, 21, 21, False, 100)
+    d4 = Deportista("Mati Fernandez", 21, 22, 12, 44, False, 42)
+    equipo = [d1, d2, d3, d4]
+
+    datos = leer_datos_delegaciones("delegaciones.csv")
+
+    for dato in datos:
+        tipo_delegacion = dato["Delegacion"]
+        entrenador = input("Ingrese el nombre del entrenador: ")
+        equipo = equipo
+        moral = dato["Moral"]
+        dinero = dato["Dinero"]
+        medallas = dato["Medallas"]
+        if tipo_delegacion == "DCCrotona":
+            delegacion = DCCrotona(entrenador, equipo, medallas, moral, dinero)
+        elif tipo_delegacion == "IEEEsport":
+            delegacion = IEEEsparta(entrenador, equipo, medallas, moral, dinero)
+
+    # testeo deportistas.csv
+    datos_deportistas = leer_datos_deportistas("deportistas.csv")
