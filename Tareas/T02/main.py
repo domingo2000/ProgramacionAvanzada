f5178a50a9e7e46d55316704d5a831eb4228d1cd
DOMINGO EDWARDS <domingo.edwards@uc.cl>
2020-10-21 00:26:08 -0300

    conectada señales ventana inicio y juego

--- Tareas/T02/main.py
+++ Tareas/T02/main.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from frontend.ventana_inicio import VentanaInicio
 from frontend.ventana_ranking import VentanaRanking
+from frontend.ventana_juego import VentanaJuego
 from backend.back_ranking import ProcesadorRanking
 
 if __name__ == "__main__":
@@ -9,12 +10,16 @@ if __name__ == "__main__":
     # Instancia ventanas que se van a usar
     ventana_inicio = VentanaInicio()
     ventana_ranking = VentanaRanking()
+    ventana_juego = VentanaJuego()
 
     # Instancia procesadores back end
     procesador_ranking = ProcesadorRanking()
     # Coneccion señales entre ventanas
+    # Inicio y ranking
     ventana_inicio.senal_abrir_ventana_ranking.connect(ventana_ranking.senal_abrir_ventana_ranking)
     ventana_ranking.senal_abrir_ventana_inicio.connect(ventana_inicio.show)
+    # Inicio y Juego
+    ventana_inicio.senal_abrir_ventana_juego.connect(ventana_juego.show)
 
     # Coneccion señales back-front ranking
     ventana_ranking.senal_procesar_puntajes = procesador_ranking.senal_procesar_puntajes
