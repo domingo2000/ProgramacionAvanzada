55eecc3a26b9559dbefded91a563ee4ba4f14cbd
DOMINGO EDWARDS <domingo.edwards@uc.cl>
2020-10-22 17:44:09 -0300

    arreglados parametros

--- Tareas/T02v2/parametros.py
+++ Tareas/T02v2/parametros.py
@@ -3,47 +3,47 @@
 IMAGENES = {
     "imagen_inicio": ["sprites", "logo.png"],
     "imagen_fondo": ["sprites", "fondos", "fondo.png"],
-    "imagen_flecha_izquerda_1": ["sprites", "flechas", "left_1.png"],
+    "imagen_flecha_izquierda_1": ["sprites", "flechas", "left_1.png"],
     "imagen_flecha_derecha_1": ["sprites", "flechas", "right_1.png"],
     "imagen_flecha_arriba_1": ["sprites", "flechas", "up_1.png"],
     "imagen_flecha_abajo_1": ["sprites", "flechas", "down_1.png"],
 
-    "imagen_flecha_izquerda_2": ["sprites", "flechas", "left_2.png"],
+    "imagen_flecha_izquierda_2": ["sprites", "flechas", "left_2.png"],
     "imagen_flecha_derecha_2": ["sprites", "flechas", "right_2.png"],
     "imagen_flecha_arriba_2": ["sprites", "flechas", "up_2.png"],
     "imagen_flecha_abajo_2": ["sprites", "flechas", "down_2.png"],
 
-    "imagen_flecha_izquerda_3": ["sprites", "flechas", "left_3.png"],
+    "imagen_flecha_izquierda_3": ["sprites", "flechas", "left_3.png"],
     "imagen_flecha_derecha_3": ["sprites", "flechas", "right_3.png"],
     "imagen_flecha_arriba_3": ["sprites", "flechas", "up_3.png"],
     "imagen_flecha_abajo_3": ["sprites", "flechas", "down_3.png"],
 
-    "imagen_flecha_izquerda_4": ["sprites", "flechas", "left_4.png"],
+    "imagen_flecha_izquierda_4": ["sprites", "flechas", "left_4.png"],
     "imagen_flecha_derecha_4": ["sprites", "flechas", "right_4.png"],
     "imagen_flecha_arriba_4": ["sprites", "flechas", "up_4.png"],
     "imagen_flecha_abajo_4": ["sprites", "flechas", "down_4.png"],
 
-    "imagen_flecha_izquerda_5": ["sprites", "flechas", "left_5.png"],
+    "imagen_flecha_izquierda_5": ["sprites", "flechas", "left_5.png"],
     "imagen_flecha_derecha_5": ["sprites", "flechas", "right_5.png"],
     "imagen_flecha_arriba_5": ["sprites", "flechas", "up_5.png"],
     "imagen_flecha_abajo_5": ["sprites", "flechas", "down_5.png"],
 
-    "imagen_flecha_izquerda_6": ["sprites", "flechas", "left_6.png"],
+    "imagen_flecha_izquierda_6": ["sprites", "flechas", "left_6.png"],
     "imagen_flecha_derecha_6": ["sprites", "flechas", "right_6.png"],
     "imagen_flecha_arriba_6": ["sprites", "flechas", "up_6.png"],
     "imagen_flecha_abajo_6": ["sprites", "flechas", "down_6.png"],
 
-    "imagen_flecha_izquerda_7": ["sprites", "flechas", "left_7.png"],
+    "imagen_flecha_izquierda_7": ["sprites", "flechas", "left_7.png"],
     "imagen_flecha_derecha_7": ["sprites", "flechas", "right_7.png"],
     "imagen_flecha_arriba_7": ["sprites", "flechas", "up_7.png"],
     "imagen_flecha_abajo_7": ["sprites", "flechas", "down_7.png"],
 
-    "imagen_flecha_izquerda_8": ["sprites", "flechas", "left_8.png"],
+    "imagen_flecha_izquierda_8": ["sprites", "flechas", "left_8.png"],
     "imagen_flecha_derecha_8": ["sprites", "flechas", "right_8.png"],
     "imagen_flecha_arriba_8": ["sprites", "flechas", "up_8.png"],
     "imagen_flecha_abajo_8": ["sprites", "flechas", "down_8.png"],
 
-    "imagen_explosion_izquerda": ["sprites", "flechas", "boom_1.png"],
+    "imagen_explosion_izquierda": ["sprites", "flechas", "boom_1.png"],
     "imagen_explosion_derecha": ["sprites", "flechas", "boom_4.png"],
     "imagen_explosion_arriba": ["sprites", "flechas", "boom_3.png"],
     "imagen_explosion_abajo": ["sprites", "flechas", "boom_2.png"],
@@ -73,10 +73,10 @@ COLORES = {
 TASA_DE_REFRESCO = 0.01
 
 # Parametros Flechas
-DIRECCIONES = ["derecha", "izquerda", "arriba", "abajo"]
+DIRECCIONES = ["izquierda", "arriba", "abajo", "derecha"]
 
 FLECHA_ARRIBA = "w"
-FLECHA_IZQUERDA = "a"
+FLECHA_izquierda = "a"
 FLECHA_ABAJO = "s"
 FLECHA_DERECHA = "d"
 
@@ -102,7 +102,7 @@ DELAY_EXPLOSION = 10
 # Parametros Niveles
 NIVEL_PRINCIPIANTE = {
     "duracion": 30,
-    "tiempo_entre_pasos": 5,
+    "tiempo_entre_pasos": 1,
     "aprobacion_necesaria": 30,
 }
 NIVEL_AFICIONADO = {
