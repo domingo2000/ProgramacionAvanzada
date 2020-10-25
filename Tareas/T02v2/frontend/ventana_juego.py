9c40b49d866d54c437d1742a0c029fbfe86e9eff
DOMINGO EDWARDS <domingo.edwards@uc.cl>
2020-10-25 01:42:00 -0300

    reseteo combos al comenzar nivel

--- Tareas/T02v2/frontend/ventana_juego.py
+++ Tareas/T02v2/frontend/ventana_juego.py
@@ -37,8 +37,10 @@ class VentanaJuego(window_name, base_class):
     def comenzar(self):
         cancion = self.opciones_cancion.currentText()
         dificultad = self.opciones_dificultad.currentText()
-        print("DEBUG dificulad, cancion", dificultad, cancion)
         self.senal_cargar_nivel.emit(cancion, dificultad)
+        # Resetea los combos
+        self.actualizar_label_combo(0)
+        self.actualizar_label_combo_maximo(0)
 
     def keyPressEvent(self, event):
         tecla = event.text()
@@ -57,3 +59,9 @@ class VentanaJuego(window_name, base_class):
 
     def destruir_label(self, label):
         label.parent = None
+
+    def actualizar_label_combo(self, int):
+        self.label_combo.setText(f"Combo: {int}")
+
+    def actualizar_label_combo_maximo(self, int):
+        self.label_combo_maximo.setText(f"Combo: {int}")
