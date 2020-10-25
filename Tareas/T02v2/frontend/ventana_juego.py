f9a666d1ae2f8b574bfa2e4c289a929a2c779468
DOMINGO EDWARDS <domingo.edwards@uc.cl>
2020-10-24 23:09:44 -0300

    avance ventana juego

--- Tareas/T02v2/frontend/ventana_juego.py
+++ Tareas/T02v2/frontend/ventana_juego.py
@@ -3,9 +3,11 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import (QApplication, QMessageBox)
 from PyQt5.QtCore import pyqtSignal
-from frontend.nivel2 import VentanaNivel
-from backend.backend_nivel2 import NivelPrincipiante, NivelAficionado, NivelMaestroCumbia
+from PyQt5.QtMultimedia import QSound
+from widgets.ventana_nivel import VentanaNivel
+from widgets.niveles import NivelPrincipiante
 import parametros as p
+from backend.funciones import sleep
 
 # Cargamos el formulario usando uic
 window_name, base_class = uic.loadUiType("qt-designer-ventana_juego.ui")
@@ -13,28 +15,47 @@ window_name, base_class = uic.loadUiType("qt-designer-ventana_juego.ui")
 
 class VentanaJuego(window_name, base_class):
     senal_teclas_presionadas = pyqtSignal(object, set)
+    senal_cargar_nivel = pyqtSignal(str, str)  # Cancion, Dificultad
 
     def __init__(self):
         super().__init__()
         self.teclas_presionadas = set()
-
+        self.nivel_cargado = False
         self.setupUi(self)
         self.init_senales()
         self.init_gui()
 
     def init_senales(self):
+        # Coneccion senales propias
         self.boton_comenzar.clicked.connect(self.comenzar)
 
+        # Senales externas
+        self.senal_nivel_cargado = pyqtSignal()
+
     def init_gui(self):
-        self.nivel = NivelAficionado()
+        nivel = NivelPrincipiante()  # Nivel temporal para inicializar la ventana
         # Crea el widget del Nivel
-        self.ventana_nivel = VentanaNivel(self.nivel, self.nivel.duracion, parent=self)
+        self.crear_ventana_nivel(nivel)
+
+    def crear_ventana_nivel(self, nivel):
+        self.ventana_nivel = VentanaNivel(nivel, nivel.duracion, parent=self)
         self.ventana_nivel.setGeometry(*p.UBICACION_VENTANAS["ventana_nivel"],
                                        *p.TAMANO_VENTANAS["ventana_nivel"])
 
     def comenzar(self):
-        print("Comenzando Ronda")
+        cancion = self.opciones_cancion.currentText()
+        dificultad = self.opciones_dificultad.currentText()
+        print("DEBUG dificulad, cancion", dificultad, cancion)
+        self.senal_cargar_nivel.emit(cancion, dificultad)
+
+        print("Hola")
+        while not(self.nivel_cargado):
+            sleep(0.1)
+            print("Cargando Nivel")
+            print(f"Nivel cargado = {self.nivel_cargado}")
         self.ventana_nivel.comenzar()
+        self.ventana_nivel.show()
+        nivel_cargado = False
 
     def keyPressEvent(self, event):
         tecla = event.text()
@@ -47,3 +68,14 @@ class VentanaJuego(window_name, base_class):
                 print(f"Señal teclas presionadas: {self.teclas_presionadas}")
                 self.senal_teclas_presionadas.emit(self.ventana_nivel, self.teclas_presionadas)
                 self.teclas_presionadas = set()
+
+    def cargar_nivel(self, nivel, cancion):
+        print("Debug: Ventana_juego Cargando nivel")
+        self.ventana_nivel = VentanaNivel(nivel, nivel.duracion, parent=self)
+        self.ventana_nivel.setGeometry(*p.UBICACION_VENTANAS["ventana_nivel"],
+                                       *p.TAMANO_VENTANAS["ventana_nivel"])
+        self.ventana_nivel.cancion = cancion
+
+        # Conecta las señales con el nivel nuevo creado
+        self.senal_teclas_presionadas.connect(nivel.manejar_teclas)
+        self.nivel_cargado = True
