e6ad419f1603e26869dc2fe4a3558e8d3f7a2ce5
DOMINGO EDWARDS <domingo.edwards@uc.cl>
2020-10-29 16:19:07 -0300

    implementado buscar mejor plataforma

--- Actividades/AF04/consultas.py
+++ Actividades/AF04/consultas.py
@@ -10,8 +10,21 @@ def buscar_info_artista(plataforma, artista_seleccionado):
 
 
 def buscar_mejor_plataforma(genero, plataformas):
-    # Completar
-    pass
+    mejor_plataforma = None
+    cantidad_canciones_maxima = 0
+    cantidad_canciones_genero = 0
+    for plataforma in plataformas:
+        nombre_plataforma = plataforma.raiz.valor
+        for nodo_genero in plataforma.raiz.hijos:
+            if nodo_genero.valor == genero:
+                for nodo_album in nodo_genero.hijos:
+                    for nodo_cancion in nodo_album.hijos:
+                        cantidad_canciones_genero += 1
+        if cantidad_canciones_genero > cantidad_canciones_maxima:
+            mejor_plataforma = plataforma
+            cantidad_canciones_maxima = cantidad_canciones_genero
+        cantidad_canciones_genero = 0
+    return mejor_plataforma
 
 
 def buscar_artistas_parecidos(nombre_cancion, plataforma):
