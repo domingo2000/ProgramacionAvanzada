9489f60da250a3545e7f19ca8a8e5e79fce9ac74
DOMINGO EDWARDS <domingo.edwards@uc.cl>
2020-11-23 18:35:18 -0300

    creadas construcciones

---
+++ Tareas/T03/server/items/construcciones.py
@@ -0,0 +1,36 @@
+import json
+from funciones import crear_diccionario_costos
+with open("parametros.json") as file:
+    data = json.load(file)
+costos = data["costos"]
+
+
+class Construccion:
+
+    def __init__(self, nombre, puntos, data_costo):
+        self.nombre = nombre
+        self.puntos = puntos
+        self.costo = crear_diccionario_costos(data_costo)
+    
+    def __repr__(self):
+        return f"{self.__class__}: Puntos: {self.puntos}"
+
+
+class Carretera(Construccion):
+
+    def __init__(self):
+        super().__init__("carretera", 1, costos["carretera"])
+
+
+class Choza(Construccion):
+
+    def __init__(self):
+        super().__init__("choza", 1, costos["choza"])
+
+
+if __name__ == "__main__":
+    choza = Choza()
+    camino = Carretera()
+
+    print(choza)
+    print(camino)
