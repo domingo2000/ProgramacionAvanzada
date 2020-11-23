53153fb8fff777cecd783883e99b01e970b8ce11
DOMINGO EDWARDS <domingo.edwards@uc.cl>
2020-11-23 20:24:12 -0300

    cambiado de back_server a server

--- Tareas/T03/server/main.py
+++ Tareas/T03/server/main.py
@@ -1,10 +1,7 @@
-from back_server import BackServer
+from server import Server
 from networking import ServerNet
 
 if __name__ == "__main__":
-    back_server = BackServer()
-    net_server = ServerNet()
+    server = Server()
 
     # Conexion señales
-    back_server.senal_enviar_comando.connect(net_server.send_command)
-    back_server.senal_enviar_log.connect(net_server.log)
---
+++ Tareas/T03/server/server.py
@@ -0,0 +1,21 @@
+from PyQt5.QtCore import pyqtSignal, QObject
+from networking import ServerNet
+from banco import Banco
+
+
+class Server():
+    senal_enviar_comando = pyqtSignal(str, tuple, str)
+    senal_enviar_log = pyqtSignal(str, str, str)
+
+    def __init__(self):
+        super().__init__()
+        self.net = ServerNet()
+
+        self.comandos_servidor = {
+            "comprar_choza": self.banco.comprar_choza,
+            "comprar_carretera": self.banco.comprar_carretera,
+            "comprar_desarrollo": self.banco.comprar_desarrollo
+        }
+
+    def iniciar_partida(self):
+        pass
