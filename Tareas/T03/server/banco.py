0f8606526ee8222c31632dbc5f6897b5b3cf2769
DOMINGO EDWARDS <domingo.edwards@uc.cl>
2020-11-23 20:23:27 -0300

    creado banco

---
+++ Tareas/T03/server/banco.py
@@ -0,0 +1,15 @@
+class Banco:
+
+    def __init__(self):
+        self.comandos = {
+
+        }
+    
+    def comprar_choza(self):
+        pass
+
+    def comprar_carretera(self):
+        pass
+    
+    def comprar_desarrollo(self):
+        pass
