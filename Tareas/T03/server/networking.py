b0a0012b91a5918c8e66a7e79ba0cade70cb0c90
DOMINGO EDWARDS <domingo.edwards@uc.cl>
2020-11-23 18:33:15 -0300

    creada actualizacion de usuarios

--- Tareas/T03/server/networking.py
+++ Tareas/T03/server/networking.py
@@ -1,6 +1,7 @@
 import socket
 from threading import Thread
 import json
+import pickle
 from faker import Faker
 
 
@@ -40,16 +41,29 @@ class ServerNet:
             usuario = self.crear_usuario(socket_cliente)
             self.log(nombre_usuario=usuario, evento="Conetarse")
 
-
     def crear_usuario(self, socket_cliente):
         fake = Faker()
         usuario = fake.name()
         self.clientes[usuario] = socket_cliente
+
+        # Actualiza los usuarios de la sala de espera
+        self.actualizar_usuarios()
+
+        # Crea el thread de escucha
         thread_escucha = Thread(target=self.escuchar_cliente,
                                 args=(usuario, ),
                                 daemon=True)
         thread_escucha.start()
         return usuario
+
+    def actualizar_usuarios(self):
+        # Actualiza la interfaz de todos los usuario conectados
+        nombres_usuarios = [usuario for usuario in self.clientes]
+        for usuario in self.clientes:
+            self.send_command("actualizar_usuarios",
+                              [nombres_usuarios],
+                              usuario)
+
     def escuchar_cliente(self, usuario):
         while True:
             try:
@@ -67,29 +81,29 @@ class ServerNet:
                     data.extend(data_bloque)
 
                 data = data.decode("utf-8")
+                self.log(nombre_usuario=usuario, evento="Request", detalles=data)
             except ConnectionError:
                 desconectado = self.revisar_desconexion(usuario)
                 if desconectado:
                     break
 
     def revisar_desconexion(self, usuario):
-        socket_cliente = self.clientes[usuario]
         bytes = "".encode("utf-8")
         desconectado = False
         try:
-            self.send_bytes(bytes, socket_cliente)
+            self.send_bytes(bytes, usuario)
         except ConnectionError:
             desconectado = True
             self.log(usuario, "Desconectado")
-            # Quita al usuario de los clientes conectados
+            # Quita al usuario de los clientes conectados y actualiza los clientes
             self.clientes.pop(usuario)
+            self.actualizar_usuarios()
         return desconectado
 
-    def send_bytes(self, bytes, socket_cliente):
+    def send_bytes(self, bytes, usuario):
         """
-        Envía mensajes al cliente.
+        Envía bytes con el protocolo del enunciado al socket dado
 
-        Implementa el protocolo del enunciado
         """
         bytes = bytes
         largo_mensaje = len(bytes)
@@ -109,7 +123,19 @@ class ServerNet:
             chunk_bytes = bytes[i * 60: (i + 1) * 60]
             data.extend(chunk_bytes)
 
+        socket_cliente = self.clientes[usuario]
         socket_cliente.send(data)
+
+    def send_command(self, comando, parametros, usuario):
+        """
+        Envia un comando serializado al usuario de la forma
+        ("comando": (parametros))
+
+        los parametros son recibidos como una lista de parametros
+        """
+        tupla = (comando, parametros)
+        tupla_serializado = pickle.dumps(tupla)
+        self.send_bytes(tupla_serializado, usuario)
 
     def log(self, nombre_usuario="-", evento="-", detalles="-"):
         print(f"{nombre_usuario: ^25} | {evento: ^25} | {detalles: ^25}")
@@ -122,5 +148,14 @@ class ServerNet:
         char = ""
         print(f"{char:-^25} | {char:-^25} | {char:-^25}")
 
+
 if __name__ == "__main__":
     server = ServerNet()
+    while True:
+        print(">>>")
+        comando = input("Ingrese un comando para enviar al cliente: ")
+        str_parametros = input("Ingrese parametros separados por comas: ")
+        parametros = tuple(str_parametros.split(", "))
+        for usuario in server.clientes:
+            server.send_command(comando, parametros, usuario)
+            server.log(usuario, evento="Instruccion", detalles=comando)
