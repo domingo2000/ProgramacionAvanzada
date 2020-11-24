d702f2205975006fec1f5f30f5905483837a6c7b
DOMINGO EDWARDS <domingo.edwards@uc.cl>
2020-11-23 21:03:48 -0300

    terminado cargar mapa y modelado de nodos

--- Tareas/T03/server/mapa.py
+++ Tareas/T03/server/mapa.py
@@ -1,3 +1,5 @@
+import json
+
 
 class Nodo:
 
@@ -7,14 +9,24 @@ class Nodo:
         self.usuario_presente = None
         self.vecinos = lista_adyacencia
 
+    def __repr__(self):
+        return f"nodo_{self.id}: -> {self.vecinos}"
+
 
 class Hexagono:
 
-    def __init__(self, id, nodos):
+    def __init__(self, id, nodos={}):
         self.id = id
-        self.nodos = nodos
+
+        self.nodos = {nodo.id: nodo for nodo in nodos}
         self.num_ficha = 0
         self.materia_prima = ""
+
+    def añadir_nodo(self, nodo):
+        self.nodos[nodo.id] = nodo
+
+    def __repr__(self):
+        return f"hex_{self.id}: num_{self.num_ficha}, {self.materia_prima}"
 
 
 class Mapa:
@@ -25,6 +37,7 @@ class Mapa:
     de grafos y recorrido
     """
     def __init__(self):
+        self.dimensiones = [0, 0]
         self.nodos = {}
         self.hexagonos = {}
 
@@ -53,4 +66,55 @@ class Mapa:
 
     def agregar_hexagono(self, hexagono):
         id_hexagono = hexagono.id
-        self.hexagonos[id] = hexagono
+        self.hexagonos[id_hexagono] = hexagono
+
+    def cargar_mapa(self, archivo_json):
+        data = json.load(archivo_json)
+
+        # Setea las dimensiones
+        self.dimensiones = data["dimensiones_mapa"]
+
+        # Instancia y agrega todos los nodos
+        data_nodos = data["nodos"]
+        for id_nodo in data_nodos:
+            lista_adyacencia = data_nodos[id_nodo]
+            nodo = Nodo(id_nodo, lista_adyacencia)
+            self.agregar_nodo(nodo)
+
+        # Instancia y agrega todos los hexagonos
+        data_hexagonos = data["hexagonos"]
+        for id_hexagono in data_hexagonos:
+            id_nodos = data_hexagonos[id_hexagono]
+            hexagono = Hexagono(id_hexagono)
+
+            # Agrega todos los nodos correspondientes al hexagono
+            for id_nodo in id_nodos:
+                nodo = self.nodos[id_nodo]
+                hexagono.añadir_nodo(nodo)
+            self.agregar_hexagono(hexagono)
+
+
+if __name__ == "__main__":
+    mapa = Mapa()
+    with open("grafo.json") as file:
+        mapa.cargar_mapa(file)
+    print(mapa)
+    """
+    nodo_0 = Nodo("0", ["1", "4"])
+    nodo_1 = Nodo("1", ["5", "0"])
+    nodo_5 = Nodo("5", ["1", "10", "6"])
+    nodo_10 = Nodo("10", ["9", "5", "15"])
+    nodo_9 = Nodo("9", ["10", "14", "4"])
+    nodo_4 = Nodo("4", ["0", "9"])
+
+    hex_1 = Hexagono("1", [nodo_0,
+                         nodo_1,
+                         nodo_10,
+                         nodo_4,
+                         nodo_5,
+                         nodo_9])
+    print(hex_1)
+    for id_nodo in hex_1.nodos:
+        nodo = hex_1.nodos[id_nodo]
+        print(nodo)
+    """
